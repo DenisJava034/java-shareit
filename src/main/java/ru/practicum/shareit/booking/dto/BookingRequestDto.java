e24102468cd10d64fc8997f25891c03eb1643025
Русин Денис <denis.rusin2018@yandex.ru>
2024-09-04 10:38:53 +0300

    Проект 15 добавили анотации FutureOrPresent и Future

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.booking.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,8 +11,10 @@ import java.time.LocalDateTime;
 public class BookingRequestDto {
     @NotNull
     private Long itemId;
+    @FutureOrPresent
     @NotNull
     private LocalDateTime start;
+    @Future
     @NotNull
     private LocalDateTime end;
 }
